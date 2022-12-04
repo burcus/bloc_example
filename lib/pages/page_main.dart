6ec1b0154f337a9bd1b457e9caf639ebb0f0f4db
Burcu Soylemez <burcuussoylemez@gmail.com>
2022-12-04 23:08:01 +0300

    init bloc and add bloc builder

--- lib/pages/page_main.dart
+++ lib/pages/page_main.dart
@@ -1,3 +1,7 @@
+import 'package:bloc_example/blocs/bloc_weather.dart';
+import 'package:bloc_example/blocs/event_weather.dart';
+import 'package:bloc_example/blocs/state_weather.dart';
+import 'package:bloc_example/models/weather_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,15 +14,69 @@ class PageMain extends StatefulWidget {
 
 class _PageMainState extends State<PageMain> {
   final TextEditingController _textController = TextEditingController();
+  late BlocWeather _blocWeather;
+
+  @override
+  void initState() {
+    super.initState();
+    _blocWeather = BlocProvider.of<BlocWeather>(context);
+  }
+
+  void _getWeatherInfo() {
+    _blocWeather.add(EventWeatherGetInfo(_textController.text));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'Weather Info',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+      ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          TextField(
-            controller: _textController,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                  hintText: 'Istanbul, france, berlin...'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _getWeatherInfo,
+            child: const Text('Check Weather Info'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: BlocBuilder<BlocWeather, StateWeather>(builder: (context, state) {
+              if (state is StateWeatherInfoFetching) {
+                return const SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is StateWeatherInfoFetched) {
+                List<WeatherInfo> weathers = state.weatherInfoList;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: weathers
+                      .map((e) => Text(
+                          '${e.date.toString()} - ${e.degree}\u00b0 - ${e.status}'))
+                      .toList(),
+                );
+              } else if (state is StateWeatherFailed) {
+                return Text('There is an error: ${state.errorMessage}');
+              } else {
+                return const SizedBox(); //for initialState
+              }
+            }),
           )
         ],
       ),
